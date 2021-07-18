f487c75eeb50b0b13fad0fd81c238bf827ed95d1
Fatahillah Arhi <fatahillah.ardhi@kompas.tv>
2021-07-18 12:51:10 +0700

    Ver 3

--- app/actions.py
+++ app/actions.py
@@ -8,20 +8,21 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 # from typing import Any, Text, Dict, List
-#
+
 # from rasa_sdk import Action, Tracker
 # from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
+# from rasa_sdk.events import SlotSet
+
+# class ActionName(Action):
+
 #     def name(self) -> Text:
-#         return "action_hello_world"
-#
+#         return "action_name"
+
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
+
+#         nama = tracker.get_slot("tellName")
+#         dispatcher.utter_message("Hai {}, ada yang bisa kami bantu?".format(nama))
+
 #         return []
